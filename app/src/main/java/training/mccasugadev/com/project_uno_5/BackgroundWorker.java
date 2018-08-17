8a29e9bc53377983c05d7d9c4dff68821d0b72c7
mcasuga <spenzcasuga@gmail.com>
2018-08-18 07:54:02 +0800

    Initial commit

---
+++ app/src/main/java/training/mccasugadev/com/project_uno_5/BackgroundWorker.java
@@ -0,0 +1,33 @@
+//package training.mccasugadev.com.project_uno_5;
+//
+//import android.content.Context;
+//import android.os.AsyncTask;
+//
+//public class BackgroundWorker extends AsyncTask<String, Void, Void>{
+//
+//    Context context;
+//
+//    public BackgroundWorker(Context context) {
+//        this.context = context;
+//    }
+//
+//    @Override
+//    protected Void doInBackground(String... objects) {
+//        return null;
+//    }
+//
+//    @Override
+//    protected void onPreExecute() {
+//        super.onPreExecute();
+//    }
+//
+//    @Override
+//    protected void onPostExecute(Void o) {
+//        super.onPostExecute(o);
+//    }
+//
+//    @Override
+//    protected void onProgressUpdate(Void... values) {
+//        super.onProgressUpdate(values);
+//    }
+//}
